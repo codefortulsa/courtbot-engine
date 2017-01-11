1b650d88b60d1eacb241966ddbdc0469d0f7a3d5
Philip Kin <philip@thekinfamily.com>
2017-01-11 09:48:11 -0600

    Added message logging

--- src/twilio.js
+++ src/twilio.js
@@ -2,6 +2,7 @@ import twilio from "twilio";
 
 export function sendMessage(msg, res) {
   const twiml = new twilio.TwimlResponse();
+  console.log("Sending:", msg);
   twiml.sms(msg);
   res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end(twiml.toString());
@@ -10,6 +11,7 @@ export function sendMessage(msg, res) {
 }
 
 export function sendNonReplyMessage(phone, message, opt) {
+  console.log("Sending:", msg, " to: ", phone);
   return new Promise(function(resolve, reject) {
     var client = twilio(opt.twilioAccount, opt.twilioToken);
     client.sendMessage({to: phone, from: opt.twilioPhone, body: message}, function(err) {
