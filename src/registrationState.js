95dab3a6edf0a8fabfccbbb2ea354e04c228f7cd
Philip Kin <philip@thekinfamily.com>
2017-01-10 20:29:01 -0600

    modified registrationstate

--- src/registrationState.js
+++ src/registrationState.js
@@ -1,7 +1,9 @@
-export default registrationState = {
+const registrationState = {
   UNBOUND: 0,
   ASKED_PARTY: 1,
   ASKED_REMINDER: 2,
   REMINDING: 3,
   UNSUBSCRIBED: 4
 };
+
+export default registrationState;
