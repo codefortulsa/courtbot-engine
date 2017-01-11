04ba8efecca2186c380933f78c880f5819b14041
Philip Kin <philip@thekinfamily.com>
2017-01-10 21:24:12 -0600

    added missing dep

--- src/deprecatedAddRoutes.js
+++ src/deprecatedAddRoutes.js
@@ -1,7 +1,8 @@
 import routes from "./routes";
+import completeOptions from "./defaultOptions";
 
 export default function(app, options) {
   console.error("Deprecated, please use app.use(\"/pathForSms\", courtbot.routes(options));");
-  var completeOptions = getCompleteOptions(options);
+  var completeOptions = completeOptions(options);
   app.use(options.path, routes(options));
 }
