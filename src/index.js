a80fcb3b8c8f4d5d1a986465171560d466d9127b
Blixa Morgan <blixa@projectmakeit.com>
2017-01-29 16:48:28 -0600

    remove twilio link.  needed to be in courtbot proper

--- src/index.js
+++ src/index.js
@@ -6,4 +6,3 @@ export { default as sendDueReminders } from "./sendDueReminders";
 export { default as CheckMissingCases } from "./checkMissingCases";
 export { default as events } from "./events";
 export { default as CourtbotConversations } from "./conversation";
-import "courtbot-engine-twilio";
