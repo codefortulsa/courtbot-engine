5b9162ad2459f5abf4e19986aaa95819295bbd46
Philip Kin <philip@thekinfamily.com>
2017-02-05 18:50:00 -0600

    Fixed conversation export

--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ export { default as addRoutes } from "./deprecatedAddRoutes";
 export { default as sendDueReminders } from "./sendDueReminders";
 export { default as checkMissingCases } from "./checkMissingCases";
 export { default as events, getCaseParties, sendNonReplyMessage } from "./events";
-export { default as CourtbotConversations } from "./conversation";
+export { default as CourtbotConversation } from "./conversation";
