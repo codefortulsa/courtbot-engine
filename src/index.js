fdb2d6c6038e1cc9b99c3ceeb9900ff2950006e9
Philip Kin <philip@thekinfamily.com>
2017-02-27 22:06:14 -0600

    Fixed export

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ export { default as registrationState } from "./registrationState";
 export { default as routes } from "./routes";
 export { default as sendDueReminders } from "./sendDueReminders";
 export { default as checkMissingCases } from "./checkMissingCases";
-export { default as events, getCaseParties, sendNonReplyMessage } from "./events";
+export { default as events, getCaseParties, sendNonReplyMessage, verifyContact } from "./events";
 export { default as CourtbotConversation } from "./conversation";
